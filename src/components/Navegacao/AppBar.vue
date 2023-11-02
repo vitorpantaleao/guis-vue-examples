bbc57abfb69a93e1aae756c48404a84d9b9a96aa
Vitor Pantaleão <vitor-hmp@hotmail.com>
2023-11-02 00:40:16 -0300

    feat: adicionando imagem e nome no app bar

--- src/components/Navegacao/AppBar.vue
+++ src/components/Navegacao/AppBar.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="w-full bg-white h-20 pr-6 flex items-center justify-end">
         <div class="flex justify-end">
-            App Bar
+            <div class="profile flex items-center gap-2">
+                <div class="profile-name ml-4 text-right">
+                    <p class="text-sm font-semibold">Vitor Hugo</p>
+                    <p class="text-xs text-gray-500">Desenvolvedor Front-end</p>
+                </div>
+                <div class="profile-img h-12 w-12 rounded-full overflow-hidden">
+                    <img src="https://i.pravatar.cc/300" class="h-full w-full rounded-[inherit] object-cover" />
+                </div>
+            </div>
         </div>
     </div>
 </template>
-
-<script setup>
-</script>
