886ce1fac5249dfd1fefb8e7c0a4fa572a8be80e
Vitor Pantaleão <vitor-hmp@hotmail.com>
2023-11-02 00:34:29 -0300

    feat: cria topo de informações

---
+++ src/components/Navegacao/AppBar.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-full bg-white h-20 pr-6 flex items-center justify-end">
+        <div class="flex justify-end">
+            App Bar
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
