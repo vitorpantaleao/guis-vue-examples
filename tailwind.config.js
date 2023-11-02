b11b59e2d2e2238ee439aad16dba714b4976766e
Vitor Pantaleão <vitor-hmp@hotmail.com>
2023-11-02 00:33:29 -0300

    feat: cria variações de cores padrão

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'primary-text': '#333333',
+                'secondary-text': '#777777',
+                'primary-color': '#E31C58',
+                'secondary-color': '#333333',
+            },
+        },
+        plugins: [],
+    }
 }
 
