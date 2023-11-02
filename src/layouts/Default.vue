54f5e373229ca58a8ab630a59bb7de1db568dfd0
Vitor Pantaleão <vitor-hmp@hotmail.com>
2023-11-02 00:16:42 -0300

    feat: cria layout padrão

---
+++ src/layouts/Default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="grid grid-cols-[15rem,1fr]">
+        <h2>Menu Lateral</h2>
+        <main class="max-w-full flex-1 bg-[#F5F5F5]">
+            <p>Topo</p>
+            <div class="p-8">
+                <p>conteúdo</p>
+                <router-view />
+            </div>
+        </main>
+    </div>
+</template>
+
+<script setup>
+
+</script>
