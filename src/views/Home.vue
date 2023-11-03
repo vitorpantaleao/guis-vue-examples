a1b6223ab4e0f546cb1214a431edca6c67164096
Vitor Pantaleão <vitor-hmp@hotmail.com>
2023-11-03 12:22:35 -0300

    feat: cria pagina 404

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,42 @@
 <template>
     <div>
-        <h1>Home View</h1>
+        <h1 class="text-3xl font-bold">Vue.js Examples</h1>
+        <p class="text-base pb-8">Este é um projeto foi criado para reproduzir os 7 exemplos listados no<a class="decoration-1" href="https://vuejs.org/examples/#counter"> site do próprio Vue.js</a>. </p>
+        <div class="flex flex-wrap gap-8">
+            <div class="w-[32%] bg-gray-200 p-4 rounded-lg">
+                <h2 class="text-xl font-bold mb-2">Counter</h2>
+                <p>Um contador simples que permite aumentar e diminuir um número.</p>
+            </div>
+    
+            <div class="w-[32%] bg-gray-200 p-4 rounded-lg">
+                <h2 class="text-xl font-bold mb-2">Temperature Converter</h2>
+                <p> Um conversor de temperatura que converte entre Celsius e Fahrenheit. </p>
+            </div>
+    
+            <div class="w-[32%] bg-gray-200 p-4 rounded-lg">
+                <h2 class="text-xl font-bold mb-2">Flight Booker</h2>
+                <p> Um simulador de reserva de voos, onde é possível escolher a data de ida e volta. </p>
+            </div>
+    
+            <div class="w-[32%] bg-gray-200 p-4 rounded-lg">
+                <h2 class="text-xl font-bold mb-2">Timer</h2>
+                <p>Um cronômetro simples que permite iniciar, pausar e zerar o tempo.</p>
+            </div>
+    
+            <div class="w-[32%] bg-gray-200 p-4 rounded-lg">
+                <h2 class="text-xl font-bold mb-2">CRUD</h2>
+                <p> Um aplicativo de gerenciamento de tarefas que permite criar, ler, atualizar e excluir tarefas. </p>
+            </div>
+    
+            <div class="w-[32%] bg-gray-200 p-4 rounded-lg">
+                <h2 class="text-xl font-bold mb-2">Circle Drawer</h2>
+                <p> Um aplicativo que permite desenhar círculos em uma tela usando o mouse. </p>
+            </div>
+    
+            <div class="w-[32%] bg-gray-200 p-4 rounded-lg">
+                <h2 class="text-xl font-bold mb-2">Cells</h2>
+                <p> Um jogo da vida de Conway, onde células vivas e mortas evoluem de acordo com regras simples. </p>
+            </div>
+        </div>
     </div>
 </template>
-
-<script setup>
-
-</script>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex justify-center items-center flex-col h-[calc(100vh-150px)]">
+        <span class="text-9xl font-bold">404</span>
+        <h1 class="text-3xl font-bold">Page Not Found</h1>
+        <p class="mt-4">The page you are looking for does not exist.</p>
+
+        <router-link to="/" class="mt-4 bg-primary-color hover:bg-secondary-color text-white font-bold py-2 px-4 rounded">Go to Home</router-link>
+    </div>
+</template>
+
+<script setup>
+
+</script>
